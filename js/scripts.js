74486df9b64d31efe1b5a054a6fdeba7dd15e840
Ornella-KK <ornellakankazi@gmail.com>
2020-10-11 13:46:51 +0200

    added propmpts for delivery

--- js/scripts.js
+++ js/scripts.js
@@ -14,12 +14,12 @@ $(document).ready(function(){
 
         var inputtedFlavour=$("#flavour").val();
         var inputtedCrust=$("#crust").val();
-        var inputtedSize=$("#size").val();
+        var inputtedSize=($("#size").val());
         var inputtedToppings=$("#toppings").val();
-        var inputtedNumber=$("input#number").val();
+        var inputtedNumber=parseInt($("input#number").val());
+        
 
         var newOrder=new Order(inputtedFlavour, inputtedCrust, inputtedSize, inputtedToppings, inputtedNumber);
-        
         $("p#result").append("<li><span class='result'>" +"Flavour: "+ newOrder.flavour + "</span></li>");
         $("p#result").append("<li><span class='result'>" +"Crust: "+ newOrder.crust + "</span></li>");  
         $("p#result").append("<li><span class='result'>" +"Size: "+ newOrder.size + "</span></li>"); 
@@ -29,27 +29,15 @@ $(document).ready(function(){
     $("#first").click(function(){
         $("#ttl").show();
     })
-    $("#second").click(function(){
-        alert("Your Total Charge Is: "+total)
-        var total=function(){
-        if(inputtedSize==small){
-            return inputtedNumber*5000;
-        }
-        else if(inputtedSize==medium){
-            return inputtedNumber*7000;
-        }
-        else if(inputtedSize==large){
-            return inputtedNumber*9000;
-        }
-        else{
-            return this.number;
-        }
-    }
+    $("#fourth").click(function(){
+        prompt("Enter Your Location: ");
+        prompt("Enter Your Phone Number: ");
+        alert("Thank You For Ordering,Pizza will be there in a few")
     })
 })
 // user interface for contacts
-$("document").ready(function(){
+$(document).ready(function(){
     $("#third").click(function(){
-        alert("Thank You For Subscribing")
+        alert("Thank You For Subscribing");
     })
 })
